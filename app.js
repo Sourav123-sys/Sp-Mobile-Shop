fc2619da6ef9ab9c7b8e321b82365ef3bd0e3fed
Sourav123 <khansourav58@gmail.com>
2022-02-28 21:19:21 +0600

    search-result-add

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const url = (`https://openapi.programming-hero.com/api/phones?search=${recieveNa
 const res= await fetch(url)
 const data = await res.json()
 displayResult(data.data)
+getName.value=""
 }
 
 
@@ -20,6 +21,27 @@ displayResult(data.data)
 const displayResult =(phone)=>{
     const limited = phone.slice(0,20)
     //console.log(limited)
+    const display = document.getElementById("search-result")
+    display.textContent =""
+    limited.map(show=>{
+console.log(show)
 
-    limited.map(show=>console.log(show))
+
+//console.log(display)
+const div = document.createElement("div")
+div.innerHTML =`
+<div class="card" style="width: 18rem;">
+        <img src="${show.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Name: ${show.phone_name}</h5>
+          <h5 class="card-title">Brand: ${show.brand}</h5>
+         
+        <button onClick="phoneDetails(${show.slug})">Explore</button>
+        </div>
+      </div>
+
+`
+display.appendChild(div)
+
+    })
 }
