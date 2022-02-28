3f89a7a951cff509e2c1a335ddec32a3d979edd6
Sourav123 <khansourav58@gmail.com>
2022-03-01 02:29:38 +0600

    responsive-complete

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const url = (`https://openapi.programming-hero.com/api/phones?search=${recieveNa
 const res= await fetch(url)
 const data = await res.json()
 displayResult(data.data)
+//console.log(data)
 getName.value=""
 }
 
@@ -23,20 +24,22 @@ const displayResult =(phone)=>{
     //console.log(limited)
     const display = document.getElementById("search-result")
     display.textContent =""
+    const showDetail = document.getElementById("detail")
+showDetail.textContent=""
     limited.map(show=>{
-console.log(show)
+//console.log(show)
 
 
 //console.log(display)
 const div = document.createElement("div")
+div.classList.add("col")
 div.innerHTML =`
 <div class="card" style="width: 18rem;">
         <img src="${show.image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Name: ${show.phone_name}</h5>
           <h5 class="card-title">Brand: ${show.brand}</h5>
-         
-        <button onClick="phoneDetails(${show.slug})">Explore</button>
+          <button onClick = "details('${show.slug}')">Details</button>
         </div>
       </div>
 
@@ -45,3 +48,77 @@ display.appendChild(div)
 
     })
 }
+// details part
+
+const details = async(id) =>{
+
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    phoneDetails(data.data)
+    console.log(data.data);
+
+   }
+
+   const phoneDetails =(result)=>{
+const showDetail = document.getElementById("detail")
+showDetail.textContent=""
+const div = document.createElement("div")
+div.classList.add("col")
+div.innerHTML = `
+
+
+
+<div class="card mb-3" style="max-width:840px">
+<div class="row g-0">
+
+<div class="col-md-4">
+<img style=width:"100%" src="${result.image}" class="img-fluid rounded-start" alt="..">
+</div>
+
+<div class="col-md-8">
+<div class="card-body >
+<h5 class="card-title fw-bold text-danger">Model: ${result.name}</h5>
+<p class="card-text mb-1">Brand: ${result.brand}</p>
+<p class="card-text mb-1">Release-Date: ${result.releaseDate? result.releaseDate:"No release date found" }</p>
+<p style="color:red"class=" fw-bold mb-1">Main Features</p>
+<li>storage: ${result.mainFeatures.storage? result.mainFeatures.storage:'No data found'}</li>
+<li>Memory: ${result.mainFeatures.memory? result.mainFeatures.memory:'No data found'}</li>
+<li>chipSet: ${result.mainFeatures.chipSet? result.mainFeatures.chipSet:'No data found'}</li>
+<li>Display: ${result.mainFeatures.displaySize? result.mainFeatures.displaySize:'No data found'}</li>
+<p style="color:red"class=" fw-bold mb-1">Sensors</p>
+${result.mainFeatures.sensors? result.mainFeatures.sensors:'No data found'}
+<p style="color:red"class=" fw-bold mb-1">Others</p>
+${result.others? 
+`<li>Bluetooth:${result.others.Bluetooth} </li>
+<li>Wlan:${result.others.WLAN} </li>
+<li>GPS:${result.others.GPS} </li>
+<li>NFC:${result.others.NFC} </li>
+<li>Radio:${result.others.Radio} </li>
+<li>Bluetooth:${result.others.USB} </li>`
+    
+    :'No data found'}
+
+</div>
+
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+`
+showDetail.appendChild(div)
+   }
